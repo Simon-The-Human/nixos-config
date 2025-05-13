d3709d49399ac0ed58df7783b207c2a1e063a543
Simon_The_Human <semirle@ya.ru>
2025-05-13 19:38:26 +0300

    unpured unstable tags

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/d89fc19e";
-    nixpkgs-experimental.url = "github:nixos/nixpkgs/a39ed32a";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-experimental.url = "github:nixos/nixpkgs/master";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -27,7 +27,6 @@
       system = system;
       specialArgs = {
         inherit inputs stateVersion hostname user;
-        inherit nixpkgs-unstable nixpkgs-experimental;
       };
 
       modules = [
