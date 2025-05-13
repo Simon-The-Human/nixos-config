dbad3b89310bd03edc833b9255921c4c15fc7fdd
Simon_The_Human <semirle@ya.ru>
2025-05-13 17:28:44 +0300

    packages adding

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -19,17 +19,39 @@ in
     # Desktop apps
     anki
     # code-cursor
-    emacs
     imv
-    mattermost-desktop
     mpv
     obs-studio
     obsidian
     pavucontrol
-    teams-for-linux
-    telegram-desktop
+    alacritty
+    audacity
+    jan
+    nixos-unstable.telegram-desktop
+    mattermost-desktop
+    mmctl
+    # unstable.steam-run
+    (vivaldi.override {
+      proprietaryCodecs = true;
+      enableWidevine = false;
+    })
+    # discord
     vesktop
-    vivaldi
+    # nixos-unstable.yandex-browser
+    obs-studio
+    rofi
+    wofi
+    mpv
+    nixos-unstable.luanti
+    kdenlive
+    gparted
+    # obsidian
+    zoom-us
+    qbittorrent
+    libreoffice
+    nixos-unstable.freecad-wayland
+    yandex-disk
+    unstable.yandex-music
 
     # CLI utils
     bc
@@ -61,9 +83,31 @@ in
     zip
 
     # Coding stuff
-    openjdk23
+    allure
+    ansible
+    cachix
+    cmake
+    direnv
+    emacs
+    emacsPackages.vterm
+    emacsPackages.fira-code-mode
+    gcc
+    minikube
+    gnumake
+    graphviz
+    # jetbrains.pycharm-community-src
+    vscode
     nodejs
+    poetry
+    pyenv
+    python
+    python39
+    python310
     python311
+    python311Packages.requests
+    racket
+    sqlite
+    vim
 
     # WM stuff
     libsForQt5.xwaylandvideobridge
@@ -71,8 +115,104 @@ in
     xdg-desktop-portal-gtk
     xdg-desktop-portal-hyprland
 
+    # CLI utils
+    nixos-unstable.awscli
+    bluez
+    bluez-tools
+    brightnessctl
+    dig
+    docker-compose
+    cava
+    f3
+    fastfetch
+    fd
+    ffmpeg
+    file
+    git
+    htop
+    kafkactl
+    lazygit
+    guestfs-tools
+    light
+    lux
+    mediainfo
+    neofetch
+    nix-index
+    ntfs3g
+    openssl
+    pandoc
+    parted
+    pwgen
+    ranger
+    ripgrep
+    scrot
+    systemd
+    openssh
+    nixos-unstable.openfortivpn-webview
+    packer
+    swww
+    tree
+    unzip
+    wget
+    yt-dlp
+    nixos-unstable.yandex-cloud
+    zip
+    zram-generator
+
+    # GUI utils
+    dmenu
+    feh
+    gromit-mpx
+    imv
+    mako
+    screenkey
+    mongodb-compass
+
     # Other
     bemoji
     nix-prefetch-scripts
+
+    # WMs and stuff
+    herbstluftwm
+    hyprland
+    hyprcursor
+    seatd
+    xdg-desktop-portal-hyprland
+    polybar
+    waybar
+    xdg-utils
+
+    # Sound
+    pipewire
+    pulseaudio
+    pamixer
+
+    # Screenshotting
+    grim
+    grimblast
+    slurp
+    flameshot
+    swappy
+
+    # Other
+    openvpn
+    networkmanager-openvpn
+    # nixos-unstable.amnezia-vpn
+    openconnect
+    home-manager
+    spice-vdagent
+    libtool
+    papirus-nord
+    # (jdk17.override {
+    #   enableJavaFX = true;}
+    # )
+    # virtualbox
+    # qemu_full
+    vagrant
+    nixfmt-classic
+    openldap
+    update-systemd-resolved
+    cyrus_sasl
+    zapret
   ];
 }
--- hosts/digital_spirit/local-packages.nix
+++ hosts/digital_spirit/local-packages.nix
@@ -1,10 +1,45 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  nixos-unstable = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/nixos-unstable)
+    # reuse the current configuration
+    { config = config.nixpkgs.config; };
+  unstable = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/master)
+    # reuse the current configuration
+    { config = config.nixpkgs.config; };
+in
+{
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
   environment.systemPackages = with pkgs; [
     gcc
     kdenlive
-    # jetbrains.pycharm-professional
+    mattermost-desktop
     # jre8
     # qemu
     # quickemu
+
+    # Wayland stuff
+    xwayland
+    wl-clipboard
+    cliphist
+    nixos-unstable.nwg-look
+
+    # GPU stuff
+    amdvlk
+    # rocm-opencl-icd
+    glaxnimate
+  ];
+  fonts.packages = with pkgs; [
+    jetbrains-mono
+    noto-fonts
+    noto-fonts-emoji
+    twemoji-color-font
+    font-awesome
+    powerline-fonts
+    powerline-symbols
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
   ];
 }
