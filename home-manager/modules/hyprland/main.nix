8047f7ff1cc7044c68d236a94358d178cc4e8ce9
Simon_The_Human <semirle@ya.ru>
2025-05-15 08:22:05 +0300

    default apps' workspaces rework

--- home-manager/modules/hyprland/main.nix
+++ home-manager/modules/hyprland/main.nix
@@ -96,12 +96,13 @@
         "move 990 60,size 900 170,pin,noinitialfocus,class:(showmethekey-gtk)"
         "noborder,nofocus,class:(showmethekey-gtk)"
 
-        "workspace 3,class:(obsidian)"
-        "workspace 3,class:(zathura)"
-        "workspace 4,class:(com.obsproject.Studio)"
-        "workspace 5,class:(telegram)"
+        "workspace 1,class:(vivaldi)"
+        "workspace 2,class:(emacs)"
+        "workspace 3,class:(virt-manager)"
+        "workspace 4,class:(telegram)"
+        "workspace 4,class:(mattermost)"
         "workspace 5,class:(vesktop)"
-        "workspace 6,class:(teams-for-linux)"
+        "workspace 5,class:(yandex-music)"
 
         "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
