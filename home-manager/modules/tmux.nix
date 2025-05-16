dfca3061b0f0fca3db42e8f100044bed89c60bfb
Simon_The_Human <semirle@ya.ru>
2025-05-16 12:54:31 +0300

    removed tmux binds for nvim

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -35,9 +35,6 @@
       bind -n M-s split-window -v
       bind -n M-v split-window -h
 
-      bind -n M-o new-window -c ~/para "nvim -c 'Telescope find_files' '0 Inbox/todolist.md'"
-      bind -n M-f new-window -c ~/flake "nvim -c 'Telescope find_files' flake.nix"
-      bind -n M-n new-window -c ~/.config/nvim "nvim -c 'Telescope find_files' init.lua"
       bind -n M-Enter new-window
       bind -n M-c kill-pane
       bind -n M-q kill-window
