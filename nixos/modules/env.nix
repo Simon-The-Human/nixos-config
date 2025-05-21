1d4a497ad35693e698ef0dd0c7039fda7f1f9705
Simon_The_Human <semirle@ya.ru>
2025-05-21 18:55:34 +0300

    Revert "lil fix for editor env"
    This reverts commit a2a14c1852441404fffd1ace9d94fd185ce6a8d0.

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -1,8 +1,7 @@
 {
   environment.sessionVariables = rec {
     TERMINAL = "alacritty";
-    EDITOR = "emacsclient -nw";
-    DEFAULT_EDITOR = "emacsclient -nw";
+    EDITOR = "emacs";
     XDG_BIN_HOME = "$HOME/.local/bin";
     PATH = [
       "${XDG_BIN_HOME}"
