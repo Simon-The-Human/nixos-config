a2a14c1852441404fffd1ace9d94fd185ce6a8d0
Simon_The_Human <semirle@ya.ru>
2025-05-21 18:31:59 +0300

    lil fix for editor env

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -1,7 +1,8 @@
 {
   environment.sessionVariables = rec {
     TERMINAL = "alacritty";
-    EDITOR = "emacs";
+    EDITOR = "emacsclient -nw";
+    DEFAULT_EDITOR = "emacsclient -nw";
     XDG_BIN_HOME = "$HOME/.local/bin";
     PATH = [
       "${XDG_BIN_HOME}"
