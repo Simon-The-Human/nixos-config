9b793680bba4d771aa9d1193063a81b853bbe85e
Simon_The_Human <semirle@ya.ru>
2025-06-23 20:51:08 +0300

    java tryout to run minecraft

--- nixos/modules/java.nix
+++ nixos/modules/java.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }:
 {
   programs.java = {
-    package = pkgs.jdk17 {
-      enableJavaFX = true;
-    };
+    # package = (pkgs.jdk21.override
+    #   {
+    #   enableJavaFX = true;
+    # });
+    package = pkgs.jdk8;
     enable = true;
     binfmt = true;
   };
