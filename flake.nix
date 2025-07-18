472e1b4e4aa59d88474e4430c8edb8cb27606b74
Simon_The_Human <semirle@ya.ru>
2025-07-18 12:03:32 +0300

    aphelia format

--- flake.nix
+++ flake.nix
@@ -15,71 +15,61 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    nixpkgs-unstable,
-    nixpkgs-experimental,
-    ...
-  }@inputs:
+  outputs = { self, nixpkgs, home-manager, nixpkgs-unstable
+    , nixpkgs-experimental, ... }@inputs:
     let
       system = "x86_64-linux";
       homeStateVersion = "24.11";
       user = "simon";
-      hosts = [
-        { hostname = "digital_spirit"; stateVersion = "24.11"; }
-      ];
+      hosts = [{
+        hostname = "digital_spirit";
+        stateVersion = "24.11";
+      }];
 
       # Общая функция для создания overlays
-      mkOverlays = system: [
-        (final: prev: {
-          unstable = import inputs.nixpkgs-unstable {
-            inherit system;
-            config.allowUnfree = true;
-          };
-          experimental = import inputs.nixpkgs-experimental {
-            inherit system;
-            config.allowUnfree = true;
-          };
-        })
-      ];
-
-      makeSystem = { hostname, stateVersion }: nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit inputs user hostname stateVersion;
-        };
-
-        modules = [
-          ({ config, ... }: {
-            # Добавляем overlays в системную конфигурацию
-            nixpkgs.overlays = mkOverlays config.nixpkgs.system;
-            nixpkgs.config.allowUnfree = true;
+      mkOverlays = system:
+        [
+          (final: prev: {
+            unstable = import inputs.nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+            experimental = import inputs.nixpkgs-experimental {
+              inherit system;
+              config.allowUnfree = true;
+            };
           })
-          ./hosts/${hostname}/configuration.nix
         ];
-      };
+
+      makeSystem = { hostname, stateVersion }:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs user hostname stateVersion; };
+
+          modules = [
+            ({ config, ... }: {
+              # Добавляем overlays в системную конфигурацию
+              nixpkgs.overlays = mkOverlays config.nixpkgs.system;
+              nixpkgs.config.allowUnfree = true;
+            })
+            ./hosts/${hostname}/configuration.nix
+          ];
+        };
 
     in {
       nixosConfigurations = nixpkgs.lib.foldl' (configs: host:
         configs // {
-          "${host.hostname}" = makeSystem {
-            inherit (host) hostname stateVersion;
-          };
-        }) {} hosts;
+          "${host.hostname}" =
+            makeSystem { inherit (host) hostname stateVersion; };
+        }) { } hosts;
 
       homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
           inherit system;
           overlays = mkOverlays system;
         };
-        extraSpecialArgs = {
-          inherit inputs homeStateVersion user;
-        };
-        modules = [
-          ./home-manager/home.nix
-        ];
+        extraSpecialArgs = { inherit inputs homeStateVersion user; };
+        modules = [ ./home-manager/home.nix ];
       };
     };
 }
